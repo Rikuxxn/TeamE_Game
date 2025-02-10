45f36e04baa26e6544394dc13dc7ca65429ebe2f
Yoshida Atsushi <241326@st.yoshida-g.ac.jp>
2025-02-10 10:25:16 +0900

    bg naotta

--- password_background.cpp
+++ password_background.cpp
@@ -28,14 +28,13 @@ void InitPasswordBackGround(void)
 	D3DXCreateTextureFromFile(pDevice,
 		"data\\TEXTURE\\UIKai6.png",//テクスチャのファイル名
 		&g_pTexturePasswordBG4);
-
 	//ヒントの読み込み
 	D3DXCreateTextureFromFile(pDevice,
 		"data\\TEXTURE\\Pass_answer2.png",//テクスチャのファイル名
 		&g_pTexturePasswordBG5);
 
 	//頂点バッファの生成
-	pDevice->CreateVertexBuffer(sizeof(VERTEX_2D) * 4 * NUM_BG,
+	pDevice->CreateVertexBuffer(sizeof(VERTEX_2D) * 4/* * NUM_BG*/,
 		D3DUSAGE_WRITEONLY,
 		FVF_VERTEX_2D,
 		D3DPOOL_MANAGED,
@@ -55,10 +54,10 @@ void InitPasswordBackGround(void)
 	pVtx[2].rhw = 1.0f;
 	pVtx[3].rhw = 1.0f;
 	//頂点カラーの設定
-	pVtx[0].col = D3DCOLOR_RGBA(100, 100, 100, 255);
-	pVtx[1].col = D3DCOLOR_RGBA(100, 100, 100, 255);
-	pVtx[2].col = D3DCOLOR_RGBA(100, 100, 100, 255);
-	pVtx[3].col = D3DCOLOR_RGBA(100, 100, 100, 255);
+	pVtx[0].col = D3DXCOLOR(0.3f, 0.3f, 0.3f, 1.0f);
+	pVtx[1].col = D3DXCOLOR(0.3f, 0.3f, 0.3f, 1.0f);
+	pVtx[2].col = D3DXCOLOR(0.3f, 0.3f, 0.3f, 1.0f);
+	pVtx[3].col = D3DXCOLOR(0.3f, 0.3f, 0.3f, 1.0f);
 	//頂点バッファをアンロックする
 	g_pVtxBuffPasswordBG->Unlock();
 
@@ -70,24 +69,24 @@ void InitPasswordBackGround(void)
 		D3DPOOL_MANAGED,
 		&g_pVtxBuffPasswordBG2,
 		NULL);
-	VERTEX_2D* pVtx2;//頂点情報へのポインタ
+	//VERTEX_2D* pVtx2;//頂点情報へのポインタ
 	//頂点バッファをロックし、頂点情報へのポインタを取得
-	g_pVtxBuffPasswordBG2->Lock(0, 0, (void**)&pVtx2, 0);
+	g_pVtxBuffPasswordBG2->Lock(0, 0, (void**)&pVtx, 0);
 	//頂点座標の設定
-	pVtx2[0].pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
-	pVtx2[1].pos = D3DXVECTOR3(1280.0f, 0.0f, 0.0f);
-	pVtx2[2].pos = D3DXVECTOR3(0.0f, 720.0f, 0.0f);
-	pVtx2[3].pos = D3DXVECTOR3(1280.0f, 720.0f, 0.0f);
+	pVtx[0].pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+	pVtx[1].pos = D3DXVECTOR3(1280.0f, 0.0f, 0.0f);
+	pVtx[2].pos = D3DXVECTOR3(0.0f, 720.0f, 0.0f);
+	pVtx[3].pos = D3DXVECTOR3(1280.0f, 720.0f, 0.0f);
 	//rhwの設定
-	pVtx2[0].rhw = 1.0f;
-	pVtx2[1].rhw = 1.0f;
-	pVtx2[2].rhw = 1.0f;
-	pVtx2[3].rhw = 1.0f;
+	pVtx[0].rhw = 1.0f;
+	pVtx[1].rhw = 1.0f;
+	pVtx[2].rhw = 1.0f;
+	pVtx[3].rhw = 1.0f;
 	//頂点カラーの設定
-	pVtx2[0].col = D3DCOLOR_RGBA(255, 100, 100, 100);
-	pVtx2[1].col = D3DCOLOR_RGBA(255, 100, 100, 100);
-	pVtx2[2].col = D3DCOLOR_RGBA(255, 100, 100, 100);
-	pVtx2[3].col = D3DCOLOR_RGBA(255, 100, 100, 100);
+	pVtx[0].col = D3DXCOLOR(0.1f, 0.1f, 0.1f, 0.5f);
+	pVtx[1].col = D3DXCOLOR(0.1f, 0.1f, 0.1f, 0.5f);
+	pVtx[2].col = D3DXCOLOR(0.1f, 0.1f, 0.1f, 0.5f);
+	pVtx[3].col = D3DXCOLOR(0.1f, 0.1f, 0.1f, 0.5f);
 	//頂点バッファをアンロックする
 	g_pVtxBuffPasswordBG2->Unlock();
 
@@ -99,24 +98,24 @@ void InitPasswordBackGround(void)
 		D3DPOOL_MANAGED,
 		&g_pVtxBuffPasswordBG3,
 		NULL);
-	VERTEX_2D* pVtx3;//頂点情報へのポインタ
+	//VERTEX_2D* pVtx3;//頂点情報へのポインタ
 	//頂点バッファをロックし、頂点情報へのポインタを取得
-	g_pVtxBuffPasswordBG3->Lock(0, 0, (void**)&pVtx3, 0);
+	g_pVtxBuffPasswordBG3->Lock(0, 0, (void**)&pVtx, 0);
 	//頂点座標の設定
-	pVtx3[0].pos = D3DXVECTOR3(PASSWORDFIELD_LEFT + 7.0f, PASSWORDFIELD_TOP + 7.0f, 0.0f);	//幅200.0f
-	pVtx3[1].pos = D3DXVECTOR3(PASSWORDFIELD_RIGHT - 7.0f, PASSWORDFIELD_TOP + 7.0f, 0.0f);	//高さ500.0f
-	pVtx3[2].pos = D3DXVECTOR3(PASSWORDFIELD_LEFT + 7.0f, PASSWORDFIELD_TOP + 59.0f, 0.0f);
-	pVtx3[3].pos = D3DXVECTOR3(PASSWORDFIELD_RIGHT - 7.0f, PASSWORDFIELD_TOP + 59.0f, 0.0f);
+	pVtx[0].pos = D3DXVECTOR3(PASSWORDFIELD_LEFT + 7.0f, PASSWORDFIELD_TOP + 7.0f, 0.0f);	//幅200.0f
+	pVtx[1].pos = D3DXVECTOR3(PASSWORDFIELD_RIGHT - 7.0f, PASSWORDFIELD_TOP + 7.0f, 0.0f);	//高さ500.0f
+	pVtx[2].pos = D3DXVECTOR3(PASSWORDFIELD_LEFT + 7.0f, PASSWORDFIELD_TOP + 59.0f, 0.0f);
+	pVtx[3].pos = D3DXVECTOR3(PASSWORDFIELD_RIGHT - 7.0f, PASSWORDFIELD_TOP + 59.0f, 0.0f);
 	//rhwの設定
-	pVtx3[0].rhw = 1.0f;
-	pVtx3[1].rhw = 1.0f;
-	pVtx3[2].rhw = 1.0f;
-	pVtx3[3].rhw = 1.0f;
+	pVtx[0].rhw = 1.0f;
+	pVtx[1].rhw = 1.0f;
+	pVtx[2].rhw = 1.0f;
+	pVtx[3].rhw = 1.0f;
 	//頂点カラーの設定
-	pVtx3[0].col = D3DCOLOR_RGBA(0, 0, 0, 255);
-	pVtx3[1].col = D3DCOLOR_RGBA(0, 0, 0, 255);
-	pVtx3[2].col = D3DCOLOR_RGBA(0, 0, 0, 255);
-	pVtx3[3].col = D3DCOLOR_RGBA(0, 0, 0, 255);
+	pVtx[0].col = D3DXCOLOR(0.0f, 0.0f, 0.0f, 255);
+	pVtx[1].col = D3DXCOLOR(0.0f, 0.0f, 0.0f, 255);
+	pVtx[2].col = D3DXCOLOR(0.0f, 0.0f, 0.0f, 255);
+	pVtx[3].col = D3DXCOLOR(0.0f, 0.0f, 0.0f, 255);
 	//頂点バッファをアンロックする
 	g_pVtxBuffPasswordBG3->Unlock();
 
@@ -128,29 +127,29 @@ void InitPasswordBackGround(void)
 		D3DPOOL_MANAGED,
 		&g_pVtxBuffPasswordBG4,
 		NULL);
-	VERTEX_2D* pVtx4;//頂点情報へのポインタ
+	//VERTEX_2D* pVtx4;//頂点情報へのポインタ
 	//頂点バッファをロックし、頂点情報へのポインタを取得
-	g_pVtxBuffPasswordBG4->Lock(0, 0, (void**)&pVtx4, 0);
+	g_pVtxBuffPasswordBG4->Lock(0, 0, (void**)&pVtx, 0);
 	//頂点座標の設定
-	pVtx4[0].pos = D3DXVECTOR3(510.0f, 550.0f, 0.0f);//幅150.0f
-	pVtx4[1].pos = D3DXVECTOR3(810.0f, 550.0f, 0.0f);//高さ40.0f
-	pVtx4[2].pos = D3DXVECTOR3(510.0f, 630.0f, 0.0f);
-	pVtx4[3].pos = D3DXVECTOR3(810.0f, 630.0f, 0.0f);
+	pVtx[0].pos = D3DXVECTOR3(510.0f, 550.0f, 0.0f);//幅150.0f
+	pVtx[1].pos = D3DXVECTOR3(810.0f, 550.0f, 0.0f);//高さ40.0f
+	pVtx[2].pos = D3DXVECTOR3(510.0f, 630.0f, 0.0f);
+	pVtx[3].pos = D3DXVECTOR3(810.0f, 630.0f, 0.0f);
 	//rhwの設定
-	pVtx4[0].rhw = 1.0f;
-	pVtx4[1].rhw = 1.0f;
-	pVtx4[2].rhw = 1.0f;
-	pVtx4[3].rhw = 1.0f;
+	pVtx[0].rhw = 1.0f;
+	pVtx[1].rhw = 1.0f;
+	pVtx[2].rhw = 1.0f;
+	pVtx[3].rhw = 1.0f;
 	//頂点カラーの設定
-	pVtx4[0].col = D3DCOLOR_RGBA(255, 255, 255, 255);
-	pVtx4[1].col = D3DCOLOR_RGBA(255, 255, 255, 255);
-	pVtx4[2].col = D3DCOLOR_RGBA(255, 255, 255, 255);
-	pVtx4[3].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+	pVtx[0].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+	pVtx[1].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+	pVtx[2].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+	pVtx[3].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
 	//テクスチャ座標の設定
-	pVtx4[0].tex = D3DXVECTOR2(0.0f, 0.0f);//(u,v)
-	pVtx4[1].tex = D3DXVECTOR2(1.0f, 0.0f);
-	pVtx4[2].tex = D3DXVECTOR2(0.0f, 1.0f);
-	pVtx4[3].tex = D3DXVECTOR2(1.0f, 1.0f);
+	pVtx[0].tex = D3DXVECTOR2(0.0f, 0.0f);//(u,v)
+	pVtx[1].tex = D3DXVECTOR2(1.0f, 0.0f);
+	pVtx[2].tex = D3DXVECTOR2(0.0f, 1.0f);
+	pVtx[3].tex = D3DXVECTOR2(1.0f, 1.0f);
 	//頂点バッファをアンロックする
 	g_pVtxBuffPasswordBG4->Unlock();
 
@@ -162,29 +161,29 @@ void InitPasswordBackGround(void)
 		D3DPOOL_MANAGED,
 		&g_pVtxBuffPasswordBG5,
 		NULL);
-	VERTEX_2D* pVtx5;//頂点情報へのポインタ
+	//VERTEX_2D* pVtx5;//頂点情報へのポインタ
 	//頂点バッファをロックし、頂点情報へのポインタを取得
-	g_pVtxBuffPasswordBG5->Lock(0, 0, (void**)&pVtx5, 0);
+	g_pVtxBuffPasswordBG5->Lock(0, 0, (void**)&pVtx, 0);
 	//頂点座標の設定
-	pVtx5[0].pos = D3DXVECTOR3(850.0f, 220.0f, 0.0f);//幅360.0f
-	pVtx5[1].pos = D3DXVECTOR3(1210.0f, 220.0f, 0.0f);//高さ130.0f
-	pVtx5[2].pos = D3DXVECTOR3(850.0f, 350.0f, 0.0f);
-	pVtx5[3].pos = D3DXVECTOR3(1210.0f, 350.0f, 0.0f);
+	pVtx[0].pos = D3DXVECTOR3(850.0f, 220.0f, 0.0f);//幅360.0f
+	pVtx[1].pos = D3DXVECTOR3(1210.0f, 220.0f, 0.0f);//高さ130.0f
+	pVtx[2].pos = D3DXVECTOR3(850.0f, 350.0f, 0.0f);
+	pVtx[3].pos = D3DXVECTOR3(1210.0f, 350.0f, 0.0f);
 	//rhwの設定
-	pVtx5[0].rhw = 1.0f;
-	pVtx5[1].rhw = 1.0f;
-	pVtx5[2].rhw = 1.0f;
-	pVtx5[3].rhw = 1.0f;
+	pVtx[0].rhw = 1.0f;
+	pVtx[1].rhw = 1.0f;
+	pVtx[2].rhw = 1.0f;
+	pVtx[3].rhw = 1.0f;
 	//頂点カラーの設定
-	pVtx5[0].col = D3DCOLOR_RGBA(255, 255, 255, 255);
-	pVtx5[1].col = D3DCOLOR_RGBA(255, 255, 255, 255);
-	pVtx5[2].col = D3DCOLOR_RGBA(255, 255, 255, 255);
-	pVtx5[3].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+	pVtx[0].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+	pVtx[1].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+	pVtx[2].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+	pVtx[3].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
 	//テクスチャ座標の設定
-	pVtx5[0].tex = D3DXVECTOR2(0.0f, 0.0f);//(u,v)
-	pVtx5[1].tex = D3DXVECTOR2(1.0f, 0.0f);
-	pVtx5[2].tex = D3DXVECTOR2(0.0f, 1.0f);
-	pVtx5[3].tex = D3DXVECTOR2(1.0f, 1.0f);
+	pVtx[0].tex = D3DXVECTOR2(0.0f, 0.0f);//(u,v)
+	pVtx[1].tex = D3DXVECTOR2(1.0f, 0.0f);
+	pVtx[2].tex = D3DXVECTOR2(0.0f, 1.0f);
+	pVtx[3].tex = D3DXVECTOR2(1.0f, 1.0f);
 	//頂点バッファをアンロックする
 	g_pVtxBuffPasswordBG5->Unlock();
 }
@@ -224,6 +223,11 @@ void UninitPasswordBackGround(void)
 		g_pVtxBuffPasswordBG4->Release();
 		g_pVtxBuffPasswordBG4 = NULL;
 	}
+	if (g_pVtxBuffPasswordBG5 != NULL)
+	{
+		g_pVtxBuffPasswordBG5->Release();
+		g_pVtxBuffPasswordBG5 = NULL;
+	}
 }
 //背景の更新処理
 void UpdatePasswordBackGround(void)
@@ -233,16 +237,17 @@ void UpdatePasswordBackGround(void)
 //背景の描画処理
 void DrawPasswordBackGround(void)
 {
-	LPDIRECT3DDEVICE9 pDevice;//デバイスへのポインタ
+	//デバイスへのポインタ
+	LPDIRECT3DDEVICE9 pDevice = GetDevice();//デバイスの取得
 
-	//デバイスの取得
-	pDevice = GetDevice();
 
 	//透明
 	//頂点バッファをデータストリーム
 	pDevice->SetStreamSource(0, g_pVtxBuffPasswordBG2, 0, sizeof(VERTEX_2D));
 	//頂点フォーマットの設定
 	pDevice->SetFVF(FVF_VERTEX_2D);
+	//テクスチャの設定
+	pDevice->SetTexture(0, NULL);
 	//背景の描画
 	pDevice->DrawPrimitive(D3DPT_TRIANGLESTRIP,//プリミティブの種類
 		0,									   //描画する最初の頂点インデックス
@@ -254,6 +259,8 @@ void DrawPasswordBackGround(void)
 	pDevice->SetStreamSource(0, g_pVtxBuffPasswordBG, 0, sizeof(VERTEX_2D));
 	//頂点フォーマットの設定
 	pDevice->SetFVF(FVF_VERTEX_2D);
+	//テクスチャの設定
+	pDevice->SetTexture(0, NULL);
 	//背景の描画
 	pDevice->DrawPrimitive(D3DPT_TRIANGLESTRIP,//プリミティブの種類
 		0,									   //描画する最初の頂点インデックス
@@ -265,6 +272,8 @@ void DrawPasswordBackGround(void)
 	pDevice->SetStreamSource(0, g_pVtxBuffPasswordBG3, 0, sizeof(VERTEX_2D));
 	//頂点フォーマットの設定
 	pDevice->SetFVF(FVF_VERTEX_2D);
+	//テクスチャの設定
+	pDevice->SetTexture(0, NULL);
 	//背景の描画
 	pDevice->DrawPrimitive(D3DPT_TRIANGLESTRIP,//プリミティブの種類
 		0,									   //描画する最初の頂点インデックス
